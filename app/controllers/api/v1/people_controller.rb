5c27907b5c6a49064d32ecc4e4ed82a180127b97
Fabio Saito <fabio.h.saito@gmail.com>
2022-03-24 11:59:08 -0300

    add serializer to people controller

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,40 +1,40 @@
-class Api::V1::PeopleController < ApplicationController
+class Api::V1::PeopleController < Api::BaseController
   def index 
-    people = Person.order('created_at DESC')
-    
-    render json: {status: 'SUCCESS', data: people}, status: :ok
+    @people = Person.order(created_at: :desc)
+
+    render json: @people, each_serializer: PersonSerializer, status: :ok
   end
 
   def show 
-    person = Person.find(params[:id])
+    @person = Person.find(params[:id])
     
-    render json: {status: 'SUCCESS', data: person}, status: :ok
+    render json: @person, serializer: PersonSerializer, status: :ok
   end
 
   def create
-    person = Person.new(person_params)
+    @person = Person.new(person_params)
 
-    if person.save
-      render json: {status: 'SUCCESS', data: person}, status: :ok
+    if @person.save
+      render json: @person, serializer: PersonSerializer, status: :ok
     else
-      render json: {status: 'ERROR', data: person.errors}, status: :unprocessable_entity
+      render json: @person, adapter: :json_api, serializer: ActiveModel::Serializer::ErrorSerializer , status: :unprocessable_entity
     end
   end
 
   def destroy 
-    person = Person.find(params[:id])
-    person.destroy
+    @person = Person.find(params[:id])
+    @person.destroy
 
-    render json: {status: 'SUCESS', data: person}, status: :ok
+    render json: @person, serializer: PersonSerializer, status: :ok
   end
 
   def update 
-    person = Person.find(params[:id])
+    @person = Person.find(params[:id])
     
-    if person.update(person_params)
-      render json: { status:'SUCCESS', data: person }, status: :ok
+    if @person.update(person_params)
+      render json: @person, serializer: PersonSerializer, status: :ok
     else
-      render json: { status: 'ERROR', data: person.errors }, status: :unprocessable_entity
+      render json: @person, adapter: :json_api, serializer: ActiveModel::Serializer::ErrorSerializer , status: :unprocessable_entity
     end
   end
 
